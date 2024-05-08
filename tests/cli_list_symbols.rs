08ba89221692d4c2a85aba27383db40dd45f35fb
Cookiegigi <gcreusot@pm.me>
2024-05-08 16:21:52 +0700

    integration test : get symbols

---
+++ tests/cli_list_symbols.rs
@@ -0,0 +1,52 @@
+use std::path::Path;
+
+use assert_cmd::Command;
+use currency_converter_cli::{
+    commands::common::{create_or_update_file, supported_symbols::Symbols},
+    config::Config,
+};
+use predicates::prelude::predicate;
+
+#[test]
+fn cli_get_symbols() -> Result<(), Box<dyn std::error::Error>> {
+    let data = vec![
+        Symbols {
+            code: "EUR".to_string(),
+            name: "Euro".to_string(),
+        },
+        Symbols {
+            code: "USD".to_string(),
+            name: "United state dollard".to_string(),
+        },
+    ];
+
+    let dirpath = "./temp/test/cli_get_symbols";
+
+    std::fs::create_dir_all(dirpath).unwrap();
+
+    let path = dirpath.to_string() + "/symbols.tsv";
+    create_or_update_file(&data, Path::new(&path)).unwrap();
+
+    let config_path = dirpath.to_string() + "/config.toml";
+    let config = Config {
+        symbols_file_path: path,
+        ..Default::default()
+    };
+
+    confy::store_path(&config_path, config).unwrap();
+
+    let mut cmd = Command::cargo_bin("currency-converter-cli")?;
+
+    cmd.arg("-vvv")
+        .arg("--config-path")
+        .arg(config_path)
+        .arg("list")
+        .arg("symbols")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("code\tname"));
+
+    std::fs::remove_dir_all(dirpath).unwrap();
+
+    Ok(())
+}
