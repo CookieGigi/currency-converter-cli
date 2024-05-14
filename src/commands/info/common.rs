c7e5efcdf95b4af123c457496876602ba6348c15
Cookiegigi <gcreusot@pm.me>
2024-05-14 16:47:28 +0700

    Handle io error to display this error in info result instead of panic

--- src/commands/info/common.rs
+++ src/commands/info/common.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, time::Duration};
 
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 use crate::commands::common::load_data;
 
@@ -9,7 +9,7 @@ use super::info_config::ConfigInfo;
 use anyhow::Result;
 
 /// All type of information
-#[derive(Serialize, Debug)]
+#[derive(Debug)]
 pub enum Info {
     Config(ConfigInfo),
     Symbols(DataInfo),
@@ -17,10 +17,21 @@ pub enum Info {
 }
 
 /// Information about data
-#[derive(Serialize, Debug)]
-pub struct DataInfo {
+#[derive(Debug)]
+pub enum DataInfo {
+    Success(DataInfoSuccess),
+    Error(DataInfoError),
+}
+
+#[derive(Debug)]
+pub struct DataInfoSuccess {
     pub seconds_since_last_update: Duration,
     pub number_of_line: usize,
+}
+
+#[derive(Debug)]
+pub struct DataInfoError {
+    pub error: std::io::Error,
 }
 
 /// Get information about data
@@ -28,14 +39,19 @@ pub fn get_data_info<T>(path: &str) -> Result<DataInfo>
 where
     T: for<'de> Deserialize<'de>,
 {
-    let file_metadata = std::fs::metadata(path)?;
+    let file_metadata = std::fs::metadata(path);
 
-    let file_data = load_data::<T>(Path::new(path))?;
+    match file_metadata {
+        Ok(metadata) => {
+            let file_data = load_data::<T>(Path::new(path))?;
 
-    Ok(DataInfo {
-        seconds_since_last_update: file_metadata.modified()?.elapsed()?,
-        number_of_line: file_data.len(),
-    })
+            Ok(DataInfo::Success(DataInfoSuccess {
+                seconds_since_last_update: metadata.modified()?.elapsed()?,
+                number_of_line: file_data.len(),
+            }))
+        }
+        Err(e) => Ok(DataInfo::Error(DataInfoError { error: e })),
+    }
 }
 
 #[cfg(test)]
@@ -44,7 +60,7 @@ mod test {
 
     use serde::{Deserialize, Serialize};
 
-    use crate::commands::common::create_or_update_file;
+    use crate::commands::{common::create_or_update_file, info::common::DataInfo};
 
     #[derive(Serialize, Deserialize, Debug)]
     struct TestData {
@@ -53,7 +69,7 @@ mod test {
     }
 
     #[test]
-    fn get_data_info() {
+    fn get_data_info_success() {
         let data = vec![
             TestData {
                 code: 1,
@@ -65,18 +81,36 @@ mod test {
             },
         ];
 
-        let dirpath = "./temp/test/commands/info/common/";
+        let dirpath = "./temp/test/commands/info/common/get_data_info_success";
 
         std::fs::create_dir_all(dirpath).unwrap();
 
-        let path = dirpath.to_string() + "test.tsv";
+        let path = dirpath.to_string() + "/test.tsv";
 
         create_or_update_file(&data, Path::new(&path)).unwrap();
 
         let res = super::get_data_info::<TestData>(&path);
 
         assert!(res.is_ok());
-        assert_eq!(res.unwrap().number_of_line, 2);
+
+        assert!(matches!(res.unwrap(), DataInfo::Success(..)));
+
+        std::fs::remove_dir_all(dirpath).unwrap();
+    }
+
+    #[test]
+    fn get_data_info_error() {
+        let dirpath = "./temp/test/commands/info/common/get_data_info_error";
+
+        std::fs::create_dir_all(dirpath).unwrap();
+
+        let path = dirpath.to_string() + "/test.tsv";
+
+        let res = super::get_data_info::<TestData>(&path);
+
+        assert!(res.is_ok());
+
+        assert!(matches!(res.unwrap(), DataInfo::Error(..)));
 
         std::fs::remove_dir_all(dirpath).unwrap();
     }
